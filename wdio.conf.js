5131a9e77a048b7d6538b681b0195975c9813f11
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-01-15 11:31:49 +0200

    6666

--- wdio.conf.js
+++ wdio.conf.js
@@ -77,22 +77,22 @@ export const config = {
     }
   },
 
-  async onComplete() {
-    const reportError = new Error("Could not generate Allure report");
-    const generation = allure(["generate", "allure-results", "--clean"]);
-    return new Promise((resolve, reject) => {
-      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+  // async onComplete() {
+  //   const reportError = new Error("Could not generate Allure report");
+  //   const generation = allure(["generate", "allure-results", "--clean"]);
+  //   return new Promise((resolve, reject) => {
+  //     const generationTimeout = setTimeout(() => reject(reportError), 5000);
 
-      generation.on("exit", function (exitCode) {
-        clearTimeout(generationTimeout);
+  //     generation.on("exit", function (exitCode) {
+  //       clearTimeout(generationTimeout);
 
-        if (exitCode !== 0) {
-          return reject(reportError);
-        }
+  //       if (exitCode !== 0) {
+  //         return reject(reportError);
+  //       }
 
-        console.log("Allure report successfully generated");
-        resolve();
-      });
-    });
-  },
+  //       console.log("Allure report successfully generated");
+  //       resolve();
+  //     });
+  //   });
+  // },
 };
